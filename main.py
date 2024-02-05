ca30f2a52fabb09663946ab6249e7253a94adee0
aibromaghin <89858980+aibromaghin@users.noreply.github.com>
2024-02-04 15:21:16 -0900

    Update main.py
    increased max_char length for adjective

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ st.title("Rapper Name Generator")
 #rapper, name, food type, adjective, hobby
 name = st.sidebar.text_area(label="What is your first name", max_chars=20)
 rapper = st.sidebar.text_area(label="Who is your favorite hip hop artist?", max_chars=20)
-adjective = st.sidebar.text_area(label='What is an adjective that describes you?', max_chars=10)
+adjective = st.sidebar.text_area(label='What is an adjective that describes you?', max_chars=20)
 hobby = st.sidebar.text_area(label='What do you do for fun?', max_chars=20)
 food_type = st.sidebar.selectbox("How do you like your food?", ('Sweet', 'Salty', 'Spicy', 'Sour', 'Bitter', 'Umami'))
 
